12e289d6f300fbca8e13592e9eaf8455fe45c25e
Karan54820 <karanyadav.best@gmail.com>
2025-03-18 02:49:04 +0530

    Add subscription model and update JWT verification error handling

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -16,6 +16,7 @@ export const verifyJWT = asyncHandler(async(req, res, next) => {
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
     
         if(!user){
+            // From frotend hit the refresh-token api
             throw new ApiError(401,"Invalid Access Token")
         }
     
